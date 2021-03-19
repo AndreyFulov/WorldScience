018a4a23f4e9b4e92905730db33876b16ecbbbf1
Andrew <fufloplay@gmail.com>
2021-03-19 18:18:45 +0500

    Delete again =(

--- people/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Famous
-def list(request):
-    famous_list = Famous.objects.all()
-    context = {'famous_list': famous_list}
-    return render(request, 'people/index.html', context)
